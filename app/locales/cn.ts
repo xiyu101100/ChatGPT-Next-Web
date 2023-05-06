27c23171040f34e1183f694e03d63f6a5b485e3c
xiyu101100 <130634547+xiyu101100@users.noreply.github.com>
2023-05-06 10:01:23 +0800

    cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "现在是未授权状态，请点击左上角购买Key后再点击左下角[设置](/#/settings)按钮输入API Key。",
+     <p>现在是未授权状态，请点击<a href="https://ka.741sms.com/buy/15">购买Key</a>后再点击左下角<a href="/#/settings">设置</a>按钮输入API Key。</p>
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
