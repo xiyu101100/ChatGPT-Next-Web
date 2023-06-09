f011afe8fe313690e1bc65f45235a53311e5b111
xiyu101100 <130634547+xiyu101100@users.noreply.github.com>
2023-06-09 17:58:18 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -92,7 +92,7 @@ const cn = {
       CheckUpdate: "检查更新",
       IsChecking: "正在检查更新...",
       FoundUpdate: (x: string) => `发现新版本：${x}`,
-      GoToUpdate: "前往更新",
+      GoToUpdate: "购买chatGPT账号",
     },
     SendKey: "发送键",
     Theme: "主题",
