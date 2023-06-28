11ff1e3eef2f7816322887afd8fa4b9dee28c151
xiyu101100 <130634547+xiyu101100@users.noreply.github.com>
2023-06-28 15:20:59 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>云码ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>QQ：78619658</div>
-        <div className={styles["sidebar-sub-title"]}>微信：xiyu101100</div>
+        <div className={styles["sidebar-sub-title"]}>升级或购买GPT4plus加QQ</div>
         <span style={{textDecoration:'none', fontSize:'80%'}}>ChatGPT商城</span><a href="https://ka.741sms.com/buy/15" target="_blank" className={styles["sidebar-sub-title"]} style={{marginLeft:'5px'}}>购买</a>         
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
